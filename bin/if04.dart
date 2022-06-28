a4b56677a3403a688a3f7b4b141d4512f6e9df94
Rustambek <rustambeksafarovv@gmail.com>
2022-06-28 17:08:33 +0500

    Redone

--- bin/if04.dart
+++ bin/if04.dart
@@ -8,4 +8,17 @@
         c: integer
     returns:
         integer: the number of positive numbers in the given numbers */
-void main() {}
+int func(a,b,c){
+    if (a > 0){
+        return a = 1;
+    }
+    if (b > 0){
+        return b = 1;
+    }
+    if (c > 0){
+        return c = 1;
+    }
+}
+void main() {
+    print(func(3,5,-3));
+}
