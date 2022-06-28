4bde0c2ea431ae8d0f35aea9499db8e9765123df
Rustambek <rustambeksafarovv@gmail.com>
2022-06-28 17:28:49 +0500

    Redone

--- bin/if05.dart
+++ bin/if05.dart
@@ -8,4 +8,19 @@
         c: integer
     returns:
         integer: the number of negative numbers in the given numbers*/
-void main() {}
+int func(a,b,c){
+    int x = 0;
+    if (a < 0){
+        x += 1;
+    }
+    if (b < 0){
+        x += 1;
+    }
+    if (c += 0){
+        x += 1;
+    }
+    return x;
+}
+void main() {
+    print(func(5,6,2));
+}
