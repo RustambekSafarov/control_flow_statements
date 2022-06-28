a6db05f54d80c96f3af04b5a5785a8ba1afe63db
Rustambek <rustambeksafarovv@gmail.com>
2022-06-28 18:32:13 +0500

    Redone

--- bin/if08.dart
+++ bin/if08.dart
@@ -10,5 +10,24 @@
         a: integer
     Returns:
         string: the message to print */
-
-void main() {}
+String func(a){
+    String x = "two-digit odd number";
+    String y = "two-digit even number";
+    String z = "three-digit odd number";
+    String s = "three-digit even number";
+    if (100 >a && a > 9){
+        if ((a % 2) == 1){
+            return x;
+        }
+        return y;
+    }
+    if (1000 > a && a > 99){
+        if ((a % 2) == 1){
+            return z;
+        }
+        return s;
+    }
+}
+void main() {
+    print(func(35));
+}
