c9b80b05ecef17aa3008c1f691ab5cabd442adb1
Rustambek <rustambeksafarovv@gmail.com>
2022-06-28 18:20:24 +0500

    Redone

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,30 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+String func(a){
+    String z = "positive odd number";
+    String s = "positive even number";
+    String n = "negative odd number";
+    String m = "negative even number";
+    String k = "the number is zero";
+    if (a > 0){
+        if ((a%2)==1){
+            return z;
+        }
+        return s;
+        
+    }
+    if (a < 0){
+        if ((a % 2)==1){
+            return n;
+        }
+        return m;
+    }
+
+    if (a == 0){
+        return k;
+    }
+}
+void main() {
+    print(func(-4));
+}
