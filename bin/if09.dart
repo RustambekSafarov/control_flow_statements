4941c7dee3691815fa33e87b53da26b636a00ce9
Rustambek <rustambeksafarovv@gmail.com>
2022-06-28 18:55:08 +0500

    Redone

--- bin/if09.dart
+++ bin/if09.dart
@@ -9,5 +9,17 @@
         a: integer
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False. */
-
-void main() {}
+bool func(a){
+    int x = a%10;
+    int y = a~/10;
+    int z = y*10+x;
+    bool c = a > 0;
+    bool b = a < 0;
+    if (a >= y){
+        return c;
+    }
+    return b;
+}
+void main() {
+    print(func(34));
+}
